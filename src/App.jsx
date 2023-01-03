33658d3fa13b1edd70eee90c0701c2f27e34fc8e
Serrin <serrin.doscher53@myhunter.cuny.edu>
2023-01-03 17:20:47 -0500

    added more lobby from styling and Game List component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import MainPage from './components/MainPage'
 import Login from './components/Login'
 import CategoryPage from './components/CategoryPage'
 import LobbyForm from './components/LobbyForm'
+import GameList from './components/GameList'
+
 function App() {
   const [selectedCat, setSelectedCat] = useState("")
   const [host, setHost] = useState(false)
@@ -15,6 +17,7 @@ function App() {
           <Route path="/categories" element={ <CategoryPage setSelectedCat={setSelectedCat}/>} />
           <Route path="/login" element={<Login host={host}/>}/>
           <Route path="/lobbyform" element={ <LobbyForm selectedCat={selectedCat}/>} />
+          <Route path='/gamelist' element={<GameList />}/>
         </Routes>
         
       </div>
