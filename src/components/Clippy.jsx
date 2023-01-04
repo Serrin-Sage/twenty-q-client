b58b48f6c8bb74a10234827ff7403c027365035e
Serrin <serrin.doscher53@myhunter.cuny.edu>
2023-01-04 17:11:50 -0500

    Created our Clippy!

---
+++ src/components/Clippy.jsx
@@ -0,0 +1,15 @@
+const Clippy = () => {
+
+  return (
+    <div className="clippy-container">
+      <div className="clippy-div">
+        <img className="clippy-image" src="Clippy.png" alt="Image of clippy" />
+        <div className="speech-bubble">
+          TEST NOTIFICATION
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Clippy
