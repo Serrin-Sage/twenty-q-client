152ede693207014076f686c7e001013cb35b3f28
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-01-04 22:57:35 -0500

    added clippy transiton, message and styling

--- src/components/Clippy.jsx
+++ src/components/Clippy.jsx
@@ -1,11 +1,23 @@
-const Clippy = () => {
+import { useState } from "react"
 
+const Clippy = ({ message, setShowClippy}) => {
+  const [transition, setTransition] = useState("fade-in")
+
+  const changeClippyState = () => {
+    setTransition(null)
+    setTimeout(() => {
+      setTransition("fade-out")
+    }, 10)
+    setTimeout(() => {
+      setShowClippy(false)
+    }, 350)
+  }
   return (
-    <div className="clippy-container">
+    <div className="clippy-container" id={transition} onClick={() => changeClippyState()}>
       <div className="clippy-div">
         <img className="clippy-image" src="Clippy.png" alt="Image of clippy" />
         <div className="speech-bubble">
-          TEST NOTIFICATION
+          {message}
         </div>
       </div>
     </div>
